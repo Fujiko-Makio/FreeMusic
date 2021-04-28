421ab3071cf607f66c57eb0deff4a755ab65e03c
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-28 18:57:38 +0900

    deviseのビューファイル作成

--- db/migrate/20210428093210_create_musics.rb
+++ db/migrate/20210428093210_create_musics.rb
@@ -1,7 +1,12 @@
 class CreateMusics < ActiveRecord::Migration[6.0]
   def change
     create_table :musics do |t|
-      t.music
+      t.string :musics_name,       null: false
+      t.text :description,         null: false
+      t.integer :price,            null: false
+      t.integer :genre,            null: false
+      t.integer :instruments,      null: false
+      t.references :user,   foreign_key: true
       t.timestamps
     end
   end
