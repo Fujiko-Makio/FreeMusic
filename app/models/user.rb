e99c49c6460c2504011ec34e7dc6a3c04213714e
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-29 18:24:14 +0900

    トップページのビューファイルの調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "Full-width characters" } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters" } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'musics/index'
   devise_for :users
+  get 'musics/index'
   root to: 'musics#index'
-  #resources :musics do only: [:index]
+  #resources :musics, only: [:]
 end
