7cdcc950e03b4c727791b4cd00d22dec1f04c655
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-05-04 01:35:55 +0900

    ユーザー詳細表示機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @musics = current_user.musics
+    @user = User.find(params[:id])
+    @musics = @user.musics.includes(:user)
   end
 end
