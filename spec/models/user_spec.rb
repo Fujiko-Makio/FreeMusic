ac52d4998956e3799ede1df2301306a451f113b5
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-05-07 22:59:09 +0900

    FactoryBotの導入

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+
+  describe 'ユーザー新規登録機能' do
+
+    context 'ユーザー登録できる時' do
+    end
+
+
+
+    context 'ユーザー登録できない時' do
+
+      it 
+
+    end
+
+
+  end
+
+end
