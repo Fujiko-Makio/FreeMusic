ed7068fc70e797c9e638806d00d8091f86672ea8
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-05-08 19:41:48 +0900

    Basic認証,READMEの記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auch
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   
   private
+
+  def basic_auch
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'fujiwara' && password == '0630'
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :first_name, :last_name, :first_name_kana, :last_name_kana])
