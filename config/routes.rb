2efabb3e71528b6de31914e15d22ab76b2019dc3
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-05-08 17:26:29 +0900

    アプリ名の変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'musics#index'
   get 'musics/index', to: 'musics#search'
-  #get 'products/index'
   resources :musics
   resources :users, only: :show
   
