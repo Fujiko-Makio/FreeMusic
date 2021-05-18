db0f93d092d4984333f80ecaa6002de7f2eabf22
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-05-18 21:40:27 +0900

    musicsビューに販売価格欄の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'musics/index', to: 'musics#search'
   root to: 'musics#index'
   
-  resources :musics
+  resources :musics do
+    resources :orders, only: [:index, :create]
+  end
   resources :users, only: [:show]
 end
