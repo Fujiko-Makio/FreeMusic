e83976d2a12e06f14c602b55a7dd2c8114c22a34
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-05-26 19:19:08 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,19 @@
 class OrdersController < ApplicationController
 
   def index
+    @order = Order.new
   end
 
   def create
+    @oder = Order.new(order_params)
+    @order.save
+    return 
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price)
   end
 
 end
